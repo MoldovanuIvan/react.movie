30c559885c1a4a9a309c6d8162c03910d00946b0
Ivan Moldovanu <45737355+MoldovanuIvan@users.noreply.github.com>
2022-09-05 10:30:33 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import ReactPlayer from 'react-player'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [film, setFilm] = useState(false)
+
+    useEffect(()=>{
+        fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/2360/videos', {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': '5a2d814a-3b78-4575-bca1-b4d049dcc2c6',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                setFilm(json)
+            })
+            .catch(err => console.log(err))
+    },[])
+
+    if (!film) return null
+
+    return (
+        <div className="App">
+{/*            <ReactPlayer playing={true} url={film.items[2].url}/>*/}
+            <iframe is="x-frame-bypass" src="https://widgets.kinopoisk.ru/discovery/trailer/167560?onlyPlayer=1&autoplay=1&cover=1" width="500" height="500"></iframe>
+        </div>
+    );
 }
 
 export default App;

4926bd8aeca55827ca1873e10088ed569bf39c49
Ivan Moldovanu <45737355+MoldovanuIvan@users.noreply.github.com>
2022-10-06 10:21:10 +0300

    фцв

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,22 @@ import React from 'react'
 import styles from './Header.module.scss'
 import logo from '../../assets/images/logoMain.svg'
 import SText from "../SText";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
 
     return <div className={styles.wrapper}>
+        <NavLink to={'/'}>
             <div className={styles.logo}>
                 <img src={logo}/>
                 <SText size={30} weight={900}>{'FilmChick'}</SText>
             </div>
-            <div className={styles.nav}>
-                <SText size={20} weight={500}>{'Home'}</SText>
-                <SText size={20} weight={500}>{'Movie'}</SText>
-                <SText size={20} weight={500}>{'Series'}</SText>
-            </div>
+        </NavLink>
+        <div className={styles.nav}>
+            <SText size={20} weight={500}>{'Home'}</SText>
+            <SText size={20} weight={500}>{'Movie'}</SText>
+            <SText size={20} weight={500}>{'Series'}</SText>
+        </div>
     </div>
 }
 
